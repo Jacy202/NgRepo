05b625bf6ccf927bb41a1356d21a55a280c401d6
roxilla <jacy202@yahoo.com>
2019-06-07 13:27:57 +0100

    new update

---
+++ dom.js
@@ -0,0 +1,26 @@
+// let arr = []
+// const input = document.querySelector("#input").value
+// const button = document.querySelector("#click")
+// button.addEventListener('click',addToArray)
+// function addToArray(){
+//     arr.push(input)
+//     console.log(arr)
+// }
+
+// const botton2 = document.querySelector("#touch")
+// botton2.addEventListener('click',removeFromArray)
+// function removeFromArray() {
+//     arr.splice(0,arr.length)
+//     console.log(arr)
+// }
+
+const firstName = prompt('input your first name','')
+const lastName = prompt('input lastname','')
+// function sentence(){
+//     return firstName,lastName
+// }
+const sentence = ((firstName,lastName) => {
+
+})
+alert(`your name is ${firstName} and lastName is ${lastName}`)
+
---
+++ task2.js
@@ -0,0 +1,8 @@
+// function validate(){
+// let input = document.getElementById("input").value;
+// if (input === 'ngo'){
+//     alert ("soo successful")
+//     window.location = "card/task.html"
+// }
+// }
+
