fbea27fef9ac5008baa155f0a1bc58660e7b02fd
roxilla <jacy202@yahoo.com>
2019-06-07 12:56:39 +0100

    well added

---
+++ card/task.js
@@ -0,0 +1,27 @@
+let name = prompt('what is your name',"Name")
+let greeting = name.split(" ")
+document.querySelector("#pick").innerHTML = `welcome ${greeting}`
+
+
+let save = document.querySelector("#save")
+let array =[]
+
+let value1 = document.getDocumentByClass(".pname").value
+let value2 = document.getDocumentByClass(".price").value
+let value3 = document.getDocumentByClass(".dmethod").value
+
+save.addEventListener("click",(e)=>{
+const product = {
+    title : value1,
+    price : value2,
+    delivery : value3,
+}
+
+array.push(product)
+JSON.parse(localStorage.getItem(array))
+localStorage.setItem('product',JSON.stringify(array))
+
+// const card = document.createElement("div")
+// card.className = "prodcard"
+e.preventDefault()
+})
